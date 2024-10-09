0eec898cd5409ae874dd8229f9f479936893c189
Marko <adriadrop@gmail.com>
2024-10-09 16:48:42 +0200

    create node ts lifi action to swarm

--- components/SwapComponent.tsx
+++ components/SwapComponent.tsx
@@ -1,51 +1,171 @@
-"use client";
+import * as lifiDataTypes from '@lifi/data-types'
+import type { ContractCallsQuoteRequest, StatusResponse } from '@lifi/sdk'
+import {
+  ChainId,
+  CoinKey,
+  createConfig,
+  EVM,
+  getContractCallsQuote,
+  getStatus,
+} from '@lifi/sdk'
+import type { Address, Chain } from 'viem'
+import {
+  createWalletClient,
+  encodeFunctionData,
+  http,
+  parseAbi,
+  publicActions,
+} from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { mainnet, arbitrum, optimism, polygon, gnosis } from 'viem/chains'
+import 'dotenv/config'
+import { promptConfirm } from './utils/promptConfirm'
+import { checkTokenAllowance } from './utils/checkTokenAllowance'
+import { transformTxRequestToSendTxParams } from './utils/transformTxRequestToSendTxParams'
+import { randomBytes } from 'crypto';
 
-// try https://github.com/lifinance/sdk/blob/main/examples/node/examples/polynomialDeposit.ts
-// Can also use direct API https://docs.li.fi/li.fi-api/li.fi-api/requesting-a-quote/cross-chain-contract-calls
+const { findDefaultToken } = (lifiDataTypes as any).default
 
+const run = async () => {
+  console.info('>> Starting Swarm Demo: Create new Batch from any chain')
+  console.info('>> Initialize LiFi SDK')
 
-import { useEffect, useState } from 'react';
-import { getRoutes, RoutesRequest } from '@lifi/sdk';
+  try {
+    const privateKey = process.env.PRIVATE_KEY as Address
 
-const HomePage: React.FC = () => {
-  const [routes, setRoutes] = useState<any[]>([]);
+    // NOTE: Here we are using the private key to get the account,
+    // but you can also use a Mnemonic account - see https://viem.sh/docs/accounts/mnemonic
+    const account = privateKeyToAccount(privateKey)
 
-  useEffect(() => {
-    const fetchRoutes = async () => {
-      try {
-        const routesRequest: RoutesRequest = {
-          fromChainId: 42161, // Arbitrum
-          toChainId: 10, // Optimism
-          fromTokenAddress: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC on Arbitrum
-          toTokenAddress: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1', // DAI on Optimism
-          fromAmount: '10000000', // 10 USDC
-        };
+    const client = createWalletClient({
+      account,
+      chain: arbitrum,
+      transport: http(),
+    }).extend(publicActions)
 
-        const result = await getRoutes(routesRequest);
-        const fetchedRoutes = result.routes;
+    const switchChains = [mainnet, arbitrum, optimism, polygon, gnosis]
 
-        console.log('Fetched Routes:', fetchedRoutes);
-        setRoutes(fetchedRoutes);
-      } catch (error) {
-        console.error('Error fetching routes:', error);
-      }
-    };
+    createConfig({
+      integrator: 'lifi-sdk-example',
+      providers: [
+        EVM({
+          getWalletClient: () => Promise.resolve(client),
+          switchChain: (chainId) =>
+            Promise.resolve(
+              createWalletClient({
+                account,
+                chain: switchChains.find((chain) => {
+                  if (chain.id == chainId) {
+                    return chain
+                  }
+                }) as Chain,
+                transport: http(),
+              })
+            ),
+        }),
+      ],
+    })
 
-    fetchRoutes();
-  }, []);
+    // config for swarm
+    const config = {
+      fromChain: ChainId.ARB,
+      toChain: ChainId.DAI,
+      fromToken: findDefaultToken(CoinKey.USDCe, ChainId.ARB).address,
+      amount: '100000000000000', // BZZ amount to be sent to buy this batch?
+      swarmContractAddress: '0x45a1502382541Cd610CC9068e88727426b696293', // PostageStamp on Gnosis
+      swarmToken: '0x45a1502382541Cd610CC9068e88727426b696293', // xBZZ on Gnosis
+      swarmContractGasLimit: '1000000',
+      swarmContractAbi: [
+        'function createBatch(address _owner, uint256 _initialBalancePerChunk, uint8 _depth, uint8 _bucketDepth, bytes32 _nonce, bool _immutable) external',
+      ],
+      swarmBatchOwner: account.address,
+      swarmBatchInitialBalance: '477774720',  // lastPrice x 17280 is minimum for one day
+      swarmBatchDepth: '20',  // This gives you size
+      swarmBatchBucketDepth: '16',  // This is minimum
+      swarmBatchNonce: '0x' + randomBytes(32).toString('hex'),  // Random nonce in hex
+      swarmBatchImmutable: 'false' // Default to false
+    }
 
-  return (
-    <div>
-      <h1>Routes Information</h1>
-      <div>
-        {routes.length === 0 ? (
-          <p>Loading routes...</p>
-        ) : (
-          <pre>{JSON.stringify(routes, null, 2)}</pre>
-        )}
-      </div>
-    </div>
-  );
-};
+    const stakeTxData = encodeFunctionData({
+      abi: parseAbi(config.swarmContractAbi),
+      functionName: 'createBatch',
+      args: [config.swarmBatchOwner, config.swarmBatchInitialBalance, config.swarmBatchDepth, config.swarmBatchBucketDepth, config.swarmBatchNonce, config.swarmBatchImmutable],
+    })
 
-export default HomePage;
+    const contractCallsQuoteRequest: ContractCallsQuoteRequest = {
+      fromChain: config.fromChain,
+      fromToken: config.fromToken,
+      fromAddress: account.address,
+      toChain: config.toChain,
+      toToken: config.swarmToken,
+      toAmount: config.amount,
+      contractCalls: [
+        {
+          fromAmount: config.amount,
+          fromTokenAddress: config.swarmToken,
+          toContractAddress: config.swarmContractAddress,
+          toContractCallData: stakeTxData,
+          toContractGasLimit: config.swarmContractGasLimit,
+        },
+      ],
+    }
+    console.info(
+      '>> create contract calls quote request',
+      contractCallsQuoteRequest
+    )
+
+    const contactCallsQuoteResponse = await getContractCallsQuote(
+      contractCallsQuoteRequest
+    )
+    console.info('>> Contract Calls Quote', contactCallsQuoteResponse)
+
+    if (!(await promptConfirm('Execute Quote?'))) {
+      return
+    }
+
+    await checkTokenAllowance(contactCallsQuoteResponse, account, client)
+
+    console.info(
+      '>> Execute transaction',
+      contactCallsQuoteResponse.transactionRequest
+    )
+
+    const hash = await client.sendTransaction(
+      transformTxRequestToSendTxParams(
+        client.account,
+        contactCallsQuoteResponse.transactionRequest
+      )
+    )
+    console.info('>> Transaction sent', hash)
+
+    const receipt = await client.waitForTransactionReceipt({
+      hash,
+    })
+    console.info('>> Transaction receipt', receipt)
+
+    // wait for execution
+    let result: StatusResponse
+    do {
+      await new Promise((res) => {
+        setTimeout(() => {
+          res(null)
+        }, 5000)
+      })
+
+      result = await getStatus({
+        txHash: receipt.transactionHash,
+        bridge: contactCallsQuoteResponse.tool,
+        fromChain: contactCallsQuoteResponse.action.fromChainId,
+        toChain: contactCallsQuoteResponse.action.toChainId,
+      })
+
+      console.info('>> Status update', result)
+    } while (result.status !== 'DONE' && result.status !== 'FAILED')
+
+    console.info('>> DONE', result)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+run()
